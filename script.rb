edad8fd8c109e28a7af3f4c255ca04fe1a9048ed
Ayan Ahmed <ayanahmed@Ayans-MacBook-Air.local>
2023-11-01 12:52:13 -0500

    fix

--- script.rb
+++ script.rb
@@ -1,28 +1,27 @@
 words = ["level", "elephant", "ruby", "book", "hello"]
 
-index = 0
 
-while index < words.length
-  word = words[index]
+
+for word in words
   puts "Word: #{word}"
-  
-  # Check if it's a palindrome 
+
+  # Check if it's a palindrome
   if word.downcase == word.downcase.reverse
     is_palindrome = 'Yes'
   else
     is_palindrome = 'No'
   end
-  
-  # Check if it contains 'e' 
+
+  # Check if it contains 'e'
   if word.downcase.include?('e')
     contains_e = 'Yes'
   else
     contains_e = 'No'
   end
-  
+
   # Count the total number of characters (letters) in the word
   character_count = word.length
-  
+
   # Replace 'ruby' with 'Python' if it appears in the word using if-else
   if word.downcase.include?('ruby')
     word = word.gsub('ruby', 'Python')
@@ -32,7 +31,7 @@ while index < words.length
   puts "- Contains 'e': #{contains_e}"
   puts "- Character Count: #{character_count}"
   puts "- After Replacement: #{word}"
-  
+
   puts "\n" # Add a new line for clarity
-  index += 1
+
 end
